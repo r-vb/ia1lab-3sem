4f13c71da67ed1be67c40d7dba9890731f1a6aaf
Rahul <r-vb@users.noreply.github.com>
2023-01-08 16:02:18 +0530

    DSA LAB IA-1

--- 1-a.c
+++ 1-a.c
@@ -4,15 +4,17 @@ void swaping(int *pa, int *pb);
 
 int main()
 {
-        int a, *pa, b, *pb;
+        int a, *p, b, *q;
     printf("Enter value of 'a': ");
     scanf("%d",&a);
     printf("Enter value of 'b': ");
     scanf("%d",&b);
-    swaping(&a,&b);
+        p = &a;
+        q = &b;
+    swaping(p,q);
     printf("After Swapping--\n\n");
-    printf("Value of 'a' is: %d\n",a);
-    printf("Value of 'b' is: %d\n",b);
+    printf("Value of 'a' is: %d\n",*p);
+    printf("Value of 'b' is: %d\n",*q);
 return 0;
 }
 
---
+++ 2-a.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void largest(int *pa, int *pb);
+
+int main()
+{
+        int a, b;
+return 0;
+}
