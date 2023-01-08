d1c705a587f95024896a51871fda161ec9a335b0
Rahul <r-vb@users.noreply.github.com>
2023-01-08 14:42:42 +0530

    DSA LAB

--- 1-a.c
+++ 1-a.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
 
-void swaping(int *a, int *b);
+void swaping(int *pa, int *pb);
 
 int main()
 {
-        int a, *a, b, *b;
-    printf("");
+        int a, *pa, b, *pb;
+    printf("Enter value of 'a': ");
+    scanf("%d",&a);
+    printf("Enter value of 'b': ");
+    scanf("%d",&b);
+    swaping(&a,&b);
+    printf("After Swapping--\n");
+    printf("Value of 'a' is: %d\n",a);
+    printf("Value of 'b' is: %d\n",b);
 return 0;
+}
+
+void swaping(int *pa, int *pb){
+        int temp;
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
 }
